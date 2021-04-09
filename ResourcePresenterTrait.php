8b75092137f39b5cedd5b219b9c618a79d5a3298
basic-app <dev@basic-app.com>
2021-04-09 15:41:07 +0300

    n/a

--- ResourcePresenterTrait.php
+++ ResourcePresenterTrait.php
@@ -8,4 +8,19 @@ namespace BasicApp\RESTful;
 
 trait ResourcePresenterTrait
 {
+
+    protected $viewsNamespace;
+
+    protected function getViewsNamespace() : ?string
+    {
+        return $this->viewsNamespace;
+    }
+
+    protected function render(string $view, array $params = []) : string
+    {
+        $namespace = $this->getViewsNamespace();
+
+        return view($namespace ?  $namespace . "\\" . $view : $view, $params);
+    }
+
 }
